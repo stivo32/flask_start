a2307780f8a48bef83ab313d2482c188331fb50f
stivo32 <stivo32@mail.ru>
2018-09-25 18:43:10 +0300

    Added postreSQL environment variable and psycopg2 in requirements

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +11,7 @@ from datetime import timedelta
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # switch off flask-sqlalc_track_modif but not sqlalchemy
 app.secret_key = 'somecode'
